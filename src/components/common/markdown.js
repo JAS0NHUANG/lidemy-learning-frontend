b9d514bff69bc6577cbcede0124f8782addc106a
jas0nhuang <jas0nhuang@tuta.io>
2021-04-09 19:17:44 +0200

    add remark-gfm plugin

--- src/components/common/markdown.js
+++ src/components/common/markdown.js
@@ -2,6 +2,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 import ReactMarkdown from "react-markdown";
 import ReactMarkdownWithHtml from "react-markdown/with-html";
+import RemarkGfm from "remark-gfm";
 
 import React from "react";
 
@@ -24,6 +25,7 @@ const Markdown = ({ source, allowHtml }) => {
   const Comp = allowHtml ? ReactMarkdownWithHtml : ReactMarkdown;
   return (
     <Comp
+      plugins={[RemarkGfm]}
       source={source}
       renderers={{ code: CodeBlock }}
       linkTarget="_blank"
